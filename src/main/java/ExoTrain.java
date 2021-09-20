deadf39c94356936d4c6635bfaa8597c894a4695
Pietro Di Bello <pierodibello@gmail.com>
2021-09-20 16:55:09 +0200

    convert to ASCII a train with the locomotive and two passenger cars

--- src/main/java/ExoTrain.java
+++ src/main/java/ExoTrain.java
@@ -1,8 +1,24 @@
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
 public class ExoTrain {
+    private final String trainAsString;
+
     public ExoTrain(String trainAsString) {
+        this.trainAsString = trainAsString;
     }
 
     public String toASCIIArt() {
-        return "<HHHH";
+        return trainAsString.chars()
+                .mapToObj(c -> String.valueOf((char) c))
+                .map(c -> toCar(c))
+                .collect(joining("::"));
+                
+    }
+
+    private String toCar(String c) {
+        if (c.equals("H")) return "<HHHH";
+        return "|OOOO|";
     }
 }
--- src/test/java/ExoTrainTest.java
+++ src/test/java/ExoTrainTest.java
@@ -6,8 +6,14 @@ public class ExoTrainTest {
     //`HPP` ---> `<HHHH::|OOOO|::|OOOO|`
     
     @Test
-    void a_train_with_just_the_locomotive() {
+    void convert_to_ASCII_a_train_with_just_the_locomotive() {
         String asciiArt = new ExoTrain("H").toASCIIArt();
         assertThat(asciiArt).isEqualTo("<HHHH");
+    }
+    
+    @Test
+    void convert_to_ASCII_a_train_with_the_locomotive_and_two_passenger_cars() {
+        String asciiArt = new ExoTrain("HPP").toASCIIArt();
+        assertThat(asciiArt).isEqualTo("<HHHH::|OOOO|::|OOOO|");
     }
 }
